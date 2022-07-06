3ef280445a522516f98cf1c39e7ad674e4214027
wyz <806543499@qq.com>
2022-07-06 21:28:05 +0800

    左神 25课

---
+++ src/study/zuo_algorithm/class24/Code01_SlidingWindowMaxArray.java
@@ -0,0 +1,28 @@
+package study.zuo_algorithm.class24;
+
+import java.util.LinkedList;
+
+public class Code01_SlidingWindowMaxArray {
+
+    public static int[] getMaxWindow(int[] arr, int w) {
+        if (arr == null || w < 1 || arr.length < w) {
+            return null;
+        }
+        LinkedList<Integer> qmax = new LinkedList<Integer>();
+        int[] res = new int[arr.length - w + 1];
+        int index = 0;
+        for (int R = 0; R < arr.length; R++) {
+            while (!qmax.isEmpty() && arr[qmax.peekLast()] <= arr[R]) {
+                qmax.pollLast();
+            }
+            qmax.addLast(R);
+            if (qmax.peekFirst() == R - w) {
+                qmax.pollFirst();
+            }
+            if (R >= w - 1) {
+                res[index++] = arr[qmax.peekFirst()];
+            }
+        }
+        return res;
+    }
+}
---
+++ src/study/zuo_algorithm/class24/Code02_AllLessNumSubArray.java
@@ -0,0 +1,42 @@
+package study.zuo_algorithm.class24;
+
+import java.util.LinkedList;
+
+public class Code02_AllLessNumSubArray {
+
+    public static int num(int[] arr, int sum) {
+        if (arr == null || arr.length == 0 || sum < 0) {
+            return 0;
+        }
+        int N = arr.length;
+        int count = 0;
+        LinkedList<Integer> maxWindow = new LinkedList<>();
+        LinkedList<Integer> minWindow = new LinkedList<>();
+        int R = 0;
+        for (int L = 0; L < N; L++) {
+            while (R < N) {
+                while (!maxWindow.isEmpty() && arr[maxWindow.peekLast()] <= arr[R]) {
+                    maxWindow.pollLast();
+                }
+                maxWindow.addLast(R);
+                while (!minWindow.isEmpty() && arr[minWindow.peekLast()] >= arr[R]) {
+                    minWindow.pollLast();
+                }
+                minWindow.addLast(R);
+                if (arr[maxWindow.peekFirst()] - arr[minWindow.peekFirst()] > sum) {
+                    break;
+                } else {
+                    R++;
+                }
+            }
+            count += R - L;
+            if (maxWindow.peekFirst() == L) {
+                maxWindow.pollFirst();
+            }
+            if (minWindow.peekFirst() == L) {
+                minWindow.pollFirst();
+            }
+        }
+        return count;
+    }
+}
